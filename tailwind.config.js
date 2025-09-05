d1b1790881361100a48fbf47751fa9392b22ff81
Niket Bhatt <120054912+niketbhatt2002@users.noreply.github.com>
2025-09-05 02:05:06 -0400

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        accent: '#A21CAF',
+        background: '#F9FAFB',
+      },
+      fontFamily: {
+        display: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
